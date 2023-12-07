379aa39db96b216b696d39e506dc2b3e24209106
Crisbel-Enobay <91605851+Crisbel-Enobay@users.noreply.github.com>
2023-12-07 14:59:37 +0800

    DONE MEALFINDER

--- VanillaJS/MealFinder/script.js
+++ VanillaJS/MealFinder/script.js
@@ -8,7 +8,7 @@ const single_mealEl = document.getElementById('single-meal');
 function searchMeal(e) {
   e.preventDefault();
 
-  //   single_mealEl.innerHTML = '';
+  single_mealEl.innerHTML = '';
 
   const term = search.value;
 
@@ -41,4 +41,75 @@ function searchMeal(e) {
   }
 }
 
+// get meal id
+function getMealId(mealId) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const meal = data.meals[0];
+      addMealToDom(meal);
+    });
+}
+
+// add meal element
+function addMealToDom(meal) {
+  const ingredients = [];
+
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+      );
+    } else {
+      break;
+    }
+  }
+
+  single_mealEl.innerHTML = `
+    <div class="single-meal">
+      <h1>${meal.strMeal}</h1>
+      <img src="${meal.strMealThumb}" alt="${meal.strMealThumb}" />
+      <div class="single-meal-info">
+        ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''} 
+        ${meal.strArea ? `<p>${meal.strArea}</p>` : ''} 
+      </div>
+      <div class="main">
+        <p>${meal.strInstructions}</p>
+        <h1>Ingredients</h1>
+        <ul>
+          ${ingredients.map((ing) => `<li>${ing}</li>`).join('')}
+        </ul>
+      </div>
+    </div>
+  `;
+}
+
+// random meal
+function getRandomMeal() {
+  mealsEl.innerHTML = '';
+  resultHeading = '';
+
+  fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+    .then((res) => res.json())
+    .then((data) => {
+      const meal = data.meals[0];
+      addMealToDom(meal);
+    });
+}
+
 submit.addEventListener('submit', searchMeal);
+random.addEventListener('click', getRandomMeal);
+
+mealsEl.addEventListener('click', (e) => {
+  const mealInfo = e.composedPath().find((item) => {
+    if (item.classList) {
+      return item.classList.contains('meal-info');
+    } else {
+      return false;
+    }
+  });
+  if (mealInfo) {
+    const mealsId = mealInfo.getAttribute('data-mealID');
+    getMealId(mealsId);
+  }
+});
