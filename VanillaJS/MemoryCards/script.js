dacd2e8636ce36265537108653ba929d11eb068e
Crisbel-Enobay <91605851+Crisbel-Enobay@users.noreply.github.com>
2023-12-16 12:12:20 +0800

    done memory card

--- VanillaJS/MemoryCards/script.js
+++ VanillaJS/MemoryCards/script.js
@@ -17,20 +17,21 @@ let currentActiveCard = 0;
 const cardsEl = [];
 
 // Store card data
-const cardsData = [
-  {
-    question: 'What must a variable begin with?',
-    answer: 'A letter, $ or _',
-  },
-  {
-    question: 'What is a variable?',
-    answer: 'container for a piece of data',
-  },
-  {
-    question: 'Example of Case Sensitive Variable',
-    answer: 'thisIsAVariable',
-  },
-];
+const cardsData = getCardsData();
+// const cardsData = [
+//   {
+//     question: 'What must a variable begin with?',
+//     answer: 'A letter, $ or _',
+//   },
+//   {
+//     question: 'What is a variable?',
+//     answer: 'container for a piece of data',
+//   },
+//   {
+//     question: 'Example of Case Sensitive Variable',
+//     answer: 'thisIsAVariable',
+//   },
+// ];
 
 // Create all cards
 function createCards() {
@@ -40,6 +41,7 @@ function createCards() {
 function createCard(data, index) {
   const card = document.createElement('div');
   card.classList.add('card');
+  // card.className = 'card active';
 
   if (index === 0) {
     card.classList.add('active');
@@ -67,7 +69,16 @@ function createCard(data, index) {
 function updateCurrentText() {
   currentEl.innerText = `${currentActiveCard + 1}/${cardsEl.length}`;
 }
-
+// Get cards from local storage
+function getCardsData() {
+  const cards = JSON.parse(localStorage.getItem('cards'));
+  return cards === null ? [] : cards;
+}
+// add card to local storage
+function setCardsData(cards) {
+  localStorage.setItem('cards', JSON.stringify(cards));
+  window.location.reload();
+}
 createCards();
 // Event Listeners
 // Next
@@ -89,4 +100,30 @@ prevBtn.addEventListener('click', () => {
   }
   cardsEl[currentActiveCard].className = 'card active';
   updateCurrentText();
+});
+// Show add container
+showBtn.addEventListener('click', () => addContainer.classList.add('show'));
+// hide add container
+hideBtn.addEventListener('click', () => addContainer.classList.remove('show'));
+// Add new card
+addCardBtn.addEventListener('click', () => {
+  const question = questionEl.value;
+  const answer = answerEl.value;
+  // console.log(question, answer)
+  if (question.trim() && answer.trim()) {
+    const newCard = { question, answer };
+    createCard(newCard);
+    questionEl.value = '';
+    answerEl.value = '';
+
+    addContainer.classList.remove('show');
+    cardsData.push(newCard);
+    setCardsData(cardsData);
+  }
+});
+// clear cards button
+clearBtn.addEventListener('click', () => {
+  localStorage.clear();
+  cardsContainer.innerHTML = '';
+  window.location.reload();
 });
